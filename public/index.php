f7832ed3b8a85b936af4cecd65dc5d9c7e30bfbc
Lluis Maria Perpiña Rofes <lperpinya1974@gmail.com>
2025-10-10 19:31:50 +0200

    Firts Commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World PHP</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p><?php echo "Hola des de PHP!"; ?></p>
+</body>
+</html>
